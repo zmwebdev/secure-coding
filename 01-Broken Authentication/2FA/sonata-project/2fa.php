19419cf389914258cbc2a59b1c414428dbb8dc27
unknown <Administrador@TOSHIBA-GD02.uni.lan>
2019-10-03 20:54:17 +0200

    2FA

--- 01-Broken Authentication/2FA/sonata-project/2fa.php
+++ 01-Broken Authentication/2FA/sonata-project/2fa.php
@@ -9,4 +9,4 @@ $g = new \Google\Authenticator\GoogleAuthenticator();
 //$g = new Sonata\GoogleAuthenticator\GoogleAuthenticator();
 $salt = '7WAO342QFANY6IKBF7L7SWEUU79WL3VMT920VB5NQMW';
 $secret = $username.$salt;
-echo '<img src="'.$g->getURL($username, 'example.com', $secret).'" />';
+echo '<img src="'.$g->getURL($username, 'uni.eus', $secret).'" />';
