ca9b487b3ecd86d5ab074ad4d40d1cd6a8f5f79b
zmwebdev <zmwebdev@gmail.com>
2019-10-04 16:47:48 +0000

    2FA pragmarx-google2fa

--- 01-Broken-Authentication/2FA/pragmarx-google2fa/2fa.php
+++ 01-Broken-Authentication/2FA/pragmarx-google2fa/2fa.php
@@ -3,10 +3,13 @@ require_once 'vendor/autoload.php';
 
 $google2fa = (new \PragmaRX\Google2FAQRCode\Google2FA());
 
+//$google2fa_secret = $google2fa->generateSecretKey();
+$google2fa_secret = $google2fa->generateSecretKey(32);  // key luzeagoa
 $inlineUrl = $google2fa->getQRCodeInline(
     'Company Name',
     'company@email.com',
-    $google2fa->generateSecretKey()
+    $google2fa_secret
 );
 
 echo '<img src="'.$inlineUrl.'" />';
+echo $google2fa_secret;
