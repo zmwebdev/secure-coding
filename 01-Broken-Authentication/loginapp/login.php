eb6054daf55d4f296a12d737592eac1eb52343b6
zmwebdev <zmwebdev@email.email>
2019-10-23 10:27:04 +0000

    login hash

--- 01-Broken-Authentication/loginapp/login.php
+++ 01-Broken-Authentication/loginapp/login.php
@@ -1,10 +1,54 @@
 <?php
 
-$user = $_POST["name"];
-//echo "<br>";
-$password =  $_POST["password"];
+$servername = "localhost";
+$username = "koxme";
+$password = "pasahitza";
+$dbname = "login";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//echo "Connected successfully";
+
+// formulariotik bidalitako datuak irakurri
+// leer desde el formulario
+$user =  $_POST['user'];
+$password = $_POST['password'];
+
+//
+$sql = "SELECT * FROM users WHERE user = '$user';";
+//echo $sql . "<br><br>";
+//
+
+$result = $conn->query($sql);
+/*
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    if ($user == $row["user"]){
+        if (password_verify($password, $row["pass"])) {
+            echo "OK";
+        } else{
+            echo "KO";
+        }
+    } else {
+        echo "KO";
+    }
+} else {
+    echo "KO";
+}
+*/
+$row = $result->fetch_assoc();
+if (($row) && ($user == $row["user"]) && (password_verify($password, $row["pass"]))) {
+    echo "OK";
+} else{
+    //echo "KO";
+    header("Location: login.html");
+}
 
 
-// mysql koxme:pasahitza
-
-
+$conn->close();
