45b392a28979c316e67a225eda123089b91be723
zmwebdev <zmwebdev@email.email>
2019-11-08 09:42:06 +0000

    2FA

--- 01-Broken-Authentication/04-2FA/pragmarx-google2fa/2FA-show-QR.php
+++ 01-Broken-Authentication/04-2FA/pragmarx-google2fa/2FA-show-QR.php
@@ -2,10 +2,23 @@
 require_once 'vendor/autoload.php';
 
 $google2fa = (new \PragmaRX\Google2FAQRCode\Google2FA());
+
+$companyName = 'Company Name';
+$companyEmail = 'company@email.com';
+
+$google2fa_secret = '3FKZAYFUU2QJXKTXOQD27RYPSGCWV3YF';
 $inlineUrl = $google2fa->getQRCodeInline(
-    'Company Name',
-    'company@email.com',
+    $companyName,
+    $companyEmail,
     $google2fa_secret
 );
 
 echo '<img src="'.$inlineUrl.'" />';
+
+$qrCodeUrl = $google2fa->getQRCodeUrl(
+    $companyName,
+    $companyEmail,
+    $secretKey
+);
+
+echo '<br>' . $qrCodeUrl;
