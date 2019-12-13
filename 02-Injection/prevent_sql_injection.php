64d94e3cd54399f73e62850f8567c2ba212b377f
zmwebdev <zmwebdev@email.email>
2019-12-13 19:08:41 +0000

    sql injection

---
+++ 02-Injection/prevent_sql_injection.php
@@ -0,0 +1,6 @@
+<?php
+
+/*
+https://www.ptsecurity.com/ww-en/analytics/knowledge-base/how-to-prevent-sql-injection-attacks/
+https://stackoverflow.com/questions/60174/how-can-i-prevent-sql-injection-in-php
+*/
