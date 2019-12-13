fc9e002fa8373f203852d6ba1b49591d00e19cf7
zmwebdev <zmwebdev@email.email>
2019-12-13 08:43:06 +0000

    sql injection

--- 02-Injection/01-loginapp/sql_injection.sql
+++ 02-Injection/01-loginapp/sql_injection.sql
@@ -6,12 +6,20 @@ use sql_injection;
 
 CREATE TABLE IF NOT EXISTS users (
     user VARCHAR(50) NOT NULL PRIMARY KEY,
-    pass VARCHAR(250)
+    pass VARCHAR(250),
+    admin INT
 );
+
+CREATE TABLE IF NOT EXISTS salary (
+    user VARCHAR(50) NOT NULL PRIMARY KEY,
+    amount INT
+);
+
 
 /* insert */
 /* koxme: p1 */
-INSERT INTO users (user, pass)
-VALUES ('koxme', 'p1'); 
-INSERT INTO users (user, pass)
-VALUES ('admin', 'p2'); 
+INSERT INTO users (user, pass, admin) VALUES ('koxme', 'p1', 0); 
+INSERT INTO users (user, pass, admin) VALUES ('admin', 'p2', 1); 
+
+INSERT INTO salary (user, amount) VALUES ('koxme', 2000); 
+INSERT INTO salary (user, amount) VALUES ('admin', 5000); 
---
+++ 02-Injection/01-loginapp/user_info.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+sql injection:
+
+pasahitzaren inputean:
+' OR '1'='1
+' OR 1=1--'
+*/
+
+$servername = "localhost";
+$username = "koxme";
+$password = "pasahitza";
+$dbname = "sql_injection";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//echo "Connected successfully";
+
+// formulariotik bidalitako datuak irakurri
+// leer desde el formulario
+$user =  $_POST['user'];
+
+//
+$sql = "SELECT * FROM users WHERE user = '$user'";
+//echo $sql . "<br>";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "erabiltzailea: " . $row["user"]. " / " . $row["pass"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
