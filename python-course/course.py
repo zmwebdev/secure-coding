8bdbd11e8a75902149fd0f971c5f8e8563099207
zmwebdev <zmwebdev@email.email>
2020-02-13 07:29:25 +0000

    python course

---
+++ python-course/course.py
@@ -0,0 +1,18 @@
+print("Kaixo Mundua!!")
+
+"Kaixo {} {}".format("Mundua","!!")
+
+s="kaixo!" + " Mundua"
+print(s)
+
+# lists
+bikes = ['trek', 'redline', 'giant']
+
+print(bikes[-1])
+
+for x in bikes:
+    print(x)
+
+for x in range(1,10):
+    print(x)
+
